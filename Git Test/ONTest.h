775109a99580a202cf2646f70a2599e74a60168a
iMac <imac@iMacs-iMac.local>
2014-08-15 12:52:35 -0400

    added a test class

---
+++ Git Test/ONTest.h
@@ -0,0 +1,13 @@
+//
+//  ONTest.h
+//  Git Test
+//
+//  Created by iMac on 8/15/14.
+//  Copyright (c) 2014 Oscar Navarro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ONTest : NSObject
+
+@end
---
+++ Git Test/ONTest.m
@@ -0,0 +1,13 @@
+//
+//  ONTest.m
+//  Git Test
+//
+//  Created by iMac on 8/15/14.
+//  Copyright (c) 2014 Oscar Navarro. All rights reserved.
+//
+
+#import "ONTest.h"
+
+@implementation ONTest
+
+@end
